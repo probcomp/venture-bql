e805a8b944e4f0b8248f7e6408950e49142e060f
Alexey Radul <axch@mit.edu>
2016-08-03 15:14:53 -0400

    Clean up test.sh a little.

--- test.sh
+++ test.sh
@@ -11,6 +11,8 @@ go ()
 	venture ${1+"$@"}
 }
 
+# These two example invocations produce the same output
+
 go <<EOF
 load_plugin plugin.py
 assume bdb = bayesdb_open("foo.bdb")
@@ -24,15 +26,3 @@ assume bdb = bayesdb_open("foo.bdb")
 assume rows = @{bql (bdb) { select * from sqlite_master; }};
 print(run(sample rows))
 EOF
-
-# print(sample rows)
-# //define rows = run(sample bayesdb_bql(pop, "select * from sqlite_master"))
-# //define begin = proc(x, y) { y() }
-# //for_each(rows, proc(row) {
-# //  begin(print("row:"), proc() {
-# //    for_each(row, proc(value) {
-# //      begin(print(" "), proc() { print(value) })
-# //    })
-# //  })
-# //})
-
